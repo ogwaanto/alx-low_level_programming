3ee48cfeba20c86f9db90f36a1eac47f6a313841
ogwaanto <ogwaanto@gmail.com>
2022-10-14 14:36:52 -0700

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,13 +4,12 @@
  *
  * My puts task
  *
- * return: return 0 after programme execution
+ * return - return 0 after programme execution
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	
+	puts("\"Programming is like building a multilingual puzzle");	
 	return (0);
 }
 
