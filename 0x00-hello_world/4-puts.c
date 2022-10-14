7487dc6bba7e3fb6e9bfdc77ce0db5653fd9cd4f
ogwaanto <ogwaanto@gmail.com>
2022-10-14 13:56:31 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * Main - entry point
+ *
+ * Return: 0 after printing function
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
+
