a2a4ba67392743b4cc78872f0080075a400f4018
ogwaanto <ogwaanto@gmail.com>
2022-10-14 04:42:04 -0700

    Task 1

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,12 +1,21 @@
-# include <stdio.h> 
+#include <stdio.h>
+
 
 
 /**
- * main - Entry point
  *
- * Return: Always 0 (Success)
- */
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
+
 {
-	Return (0);
+
+	    return (0);
+
 }
