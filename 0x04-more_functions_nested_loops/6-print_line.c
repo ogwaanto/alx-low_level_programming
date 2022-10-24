b06eb026db9c653acae1e478ff541a595e6621b2
ogwaanto <ogwaanto@gmail.com>
2022-10-24 15:40:02 -0700

    Task

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *  * print_line - prints a line
- *   * @n: variable
- *     * Return: 0
- *      */
+ * print_line - prints a line
+ * @n: variable
+ * Return: 0
+ */
 
 void print_line(int n)
 {
