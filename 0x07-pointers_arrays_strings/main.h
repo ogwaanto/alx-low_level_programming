3b1369933684766033133f84a4c9c4a94683be8d
ogwaanto <ogwaanto@gmail.com>
2022-12-02 15:40:15 -0800

    TAsk 1

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,7 +9,7 @@ int _putchar(char c);
 
 char *_memset(char *s, char b, unsigned int n);
 
-void *_memcpy(void *dest, const void *src, size_t n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 char *_strchr(char *s, char c);
 
