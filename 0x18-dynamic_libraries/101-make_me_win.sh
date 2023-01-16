d7f60088e3d1270a77d3fa1251485cfb62ec81ce
ogwaanto <ogwannto@gmail.com>
2023-01-16 20:01:39 +0300

    hot fix

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/ogwaanto/alx-low_level_programming/blob/5503be93b5b4d22ca274cc6aef2b0153cdbb3dcd/0x18-dynamic_libraries/hacked.so
+wget -P /tmp https://github.com/ogwaanto/alx-low_level_programming/0x18-dynamic_libraries/hacked.so
 export LD_PRELOAD=/tmp/hacked.so
